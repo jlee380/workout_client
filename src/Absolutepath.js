df6cf304379bd696febc3389bbe61f8023df5f54
Jonghun Lee <jonghunjhl@gmail.com>
2019-09-29 18:02:16 -0400

    no message

--- src/Absolutepath.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Absolutepath extends Component {
-    render() {
-        return <div>HI this is Absolutepath</div>;
-    }
-}
--- src/components/partner/Gym.js
+++ src/components/partner/Gym.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import Absolutepath from 'Absolutepath';
 
 export default class Gym extends Component {
     render() {
-        return <div>{<Absolutepath></Absolutepath>}</div>;
+        return <div>hi</div>;
     }
 }
