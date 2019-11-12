1900ffc4ccaf2c9a1da3a1e6b1273862b3949bf0
Jonghun Lee <jonghunjhl@gmail.com>
2019-11-12 11:34:26 -0500

    Save the state to localStorage to keep data after refreshing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
-import fire from 'config/firebase';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Wrapper from './layout/Wrapper';
 import Navbar from './components/navbar/Navbar';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
-import Gym from './components/partner/Gym';
-import User from './components/partner/User';
-import Partner from './components/partner/Partner';
-import Signup from './components/signup/Signup';
-import Home from './components/Home';
-import UserProfileDetail from 'components/partner/UserProfileDetail';
+import Routers from './Routers';
 
 // import 'bootstrap/dist/css/bootsrap.min.css';
 
 class App extends Component {
     render() {
-        const { match } = this.props;
         return (
             <div className='app'>
-                {console.log(match, 'here')}
                 <Router>
                     <Wrapper>
                         <Navbar />
                         <Header></Header>
-                        <Route path='/' exact component={Home} />
-                        <Route path='/partner' component={Partner} />
-                        <Route path='/gym' component={Gym} />
-                        <Route path='/user' component={User} />
-                        <Route path='/signup' component={Signup} />
-                        <Route
-                            path='/partner/:uid'
-                            component={UserProfileDetail}
-                        />
+                        <Routers />
                         <Footer>footer text</Footer>
                     </Wrapper>
                 </Router>
