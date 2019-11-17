d0bcbc2bb1c3b9d1c10d678150d991a7acd76e98
Jonghun Lee <jonghunjhl@gmail.com>
2019-11-17 18:38:06 -0500

    no change

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import Wrapper from './layout/Wrapper';
-import Navbar from './components/navbar/Navbar';
-import Header from './layout/Header';
-import Footer from './layout/Footer';
-import Gym from './components/partner/Gym';
-import User from './components/partner/User';
-import Partner from './components/partner/Partner';
-import Signup from './components/signup/Signup';
+import React from "react";
+import Wrapper from "./layout/Wrapper";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Gym from "./components/partner/Gym";
+import User from "./components/partner/User";
+import Partner from "./components/partner/Partner";
+import Signup from "./components/signup/Signup";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './components/Home';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 // import 'bootstrap/dist/css/bootsrap.min.css';
 
 function App() {
-    return (
-        <div className='app'>
-            <Router>
-                <Wrapper>
-                    <Navbar />
-                    <Header></Header>
-                    <Route path='/' exact component={Home} />
-                    <Route path='/partner' component={Partner} />
-                    <Route path='/gym' component={Gym} />
-                    <Route path='/user' component={User} />
-                    <Route path='/signup' component={Signup} />
-                    <Footer>footer text</Footer>
-                </Wrapper>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="app">
+      <Router>
+        <Wrapper>
+          <Navbar />
+          <Header></Header>
+          <Route path="/" exact component={Home} />
+          <Route path="/partner" component={Partner} />
+          <Route path="/gym" component={Gym} />
+          <Route path="/user" component={User} />
+          <Route path="/signup" component={Signup} />
+          <Footer>footer text</Footer>
+        </Wrapper>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
